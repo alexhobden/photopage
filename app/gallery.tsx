e0bc4c31bc0223f2cf53e297c3edbbdb700a6480
alexhobden <alex.hobden@gmx.de>
2025-03-30 17:20:48 +0200

    Add gallery animation

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { div } from "framer-motion/client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -19,52 +20,60 @@ export default function Gallery({ fetchImage }: Props) {
   }, []);
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 flex-1 w-full px-4 lg:px-16 pt-8 lg:pt-12 mt-28 lg:mt-14 overflow-auto scrollbar-hide mb-8 gap-6 z-20">
-      {images.map((src, index) => (
-        <motion.div
-          key={index}
-          whileHover={{ scale: 1.05 }}
-          transition={{
-            ease: "easeInOut",
-          }}
-          className=" aspect-[2/3] group "
-          onClick={() => {
-            setIsFlipping(true);
-            fetchImage(src.split("/").pop() || "");
-            setTimeout(() => {
-              setIsFlipping(false);
-            }, 1000); // Adjust the timeout to match your animation duration
-          }}
-        >
-          <Image
-            src={src}
-            alt={`Gallery ${index}`}
-            width={200}
-            height={300}
-            objectFit="cover"
-          />
+    <div className="relative overflow-y-scroll h-full scrollbar-hide z-0 mb-8">
+      <motion.div
+        initial={{ y: 800 }}
+        animate={{ y: 0 }}
+        exit={{ y: 800 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+        className="grid grid-cols-2 lg:grid-cols-4 flex-1 w-full px-4 lg:px-16 pt-8 lg:pt-12 mt-28 lg:mt-14   gap-6 z-20"
+      >
+        {images.map((src, index) => (
           <motion.div
-            initial={{ scale: 1 }}
-            animate={isFlipping ? { scale: [1, 1.3, 1.3, 1] } : {}}
+            key={index}
+            whileHover={{ scale: 1.05 }}
             transition={{
-              times: [0, 0.1, 1, 1],
-              duration: 1,
-              ease: [0.2, 0.8, 0.2, 1],
+              ease: "easeInOut",
             }}
-            className="hidden group-hover:block border-white  w-[15%] aspect-square border-b-[0.8px] border-l-[0.8px] -bottom-2 -left-2 lg:absolute"
-          />
-          <motion.div
-            initial={{ scale: 1 }}
-            animate={isFlipping ? { scale: [1, 1.3, 1.3, 1] } : {}}
-            transition={{
-              times: [0, 0.1, 0.9, 1],
-              duration: 1,
-              ease: [0.2, 0.8, 0.2, 1],
+            className=" aspect-[2/3] group "
+            onClick={() => {
+              setIsFlipping(true);
+              fetchImage(src.split("/").pop() || "");
+              setTimeout(() => {
+                setIsFlipping(false);
+              }, 1000); // Adjust the timeout to match your animation duration
             }}
-            className="hidden group-hover:block  border-white  w-[15%] aspect-square border-t-[0.8px] border-r-[0.8px] -top-2 -right-2 lg:absolute"
-          ></motion.div>
-        </motion.div>
-      ))}
+          >
+            <Image
+              src={src}
+              alt={`Gallery ${index}`}
+              width={200}
+              height={300}
+              objectFit="cover"
+            />
+            <motion.div
+              initial={{ scale: 1 }}
+              animate={isFlipping ? { scale: [1, 1.3, 1.3, 1] } : {}}
+              transition={{
+                times: [0, 0.1, 1, 1],
+                duration: 1,
+                ease: [0.2, 0.8, 0.2, 1],
+              }}
+              className="hidden group-hover:block border-white  w-[15%] aspect-square border-b-[0.8px] border-l-[0.8px] -bottom-2 -left-2 lg:absolute"
+            />
+            <motion.div
+              initial={{ scale: 1 }}
+              animate={isFlipping ? { scale: [1, 1.3, 1.3, 1] } : {}}
+              transition={{
+                times: [0, 0.1, 0.9, 1],
+                duration: 1,
+                ease: [0.2, 0.8, 0.2, 1],
+              }}
+              className="hidden group-hover:block  border-white  w-[15%] aspect-square border-t-[0.8px] border-r-[0.8px] -top-2 -right-2 lg:absolute"
+            ></motion.div>
+          </motion.div>
+        ))}
+      </motion.div>
     </div>
   );
 }
--- app/right-section.tsx
+++ app/right-section.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Play, Pause, Shuffle, LayoutGrid } from "lucide-react";
 import Gallery from "./gallery";
 
@@ -155,8 +155,9 @@ export const RightSection = ({
 
   return (
     <div className="absolute lg:relative  flex flex-col justify-end items-center lg:pb-8   h-full w-full lg:w-auto flex-1 font-glasgow z-30">
-      {showGallery && <Gallery fetchImage={fetchImage} />}
-
+      <AnimatePresence>
+        {showGallery && <Gallery fetchImage={fetchImage} />}
+      </AnimatePresence>
       <div className="flex gap-12 lg:pb-auto pb-4">
         <button
           onClick={() => {
